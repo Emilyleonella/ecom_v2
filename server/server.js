71590065a75e2825d752b5ae0b274d26ae753223
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-22 21:03:07 -0400

    new

--- server/server.js
+++ server/server.js
@@ -12,11 +12,13 @@ const { type } = require('os');
 
 // app.listen(port, () => console.log(`App is live on port ${port}!`));
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Leonella123$',
-  database: 'Ecom_V2'
+//mysql://be1a0f798982d8:d8d52a08@us-cdbr-east-05.cleardb.net/heroku_8042a9edba81ab0?reconnect=true
+
+const db = mysql.createPool({
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'be1a0f798982d8',
+  password: 'd8d52a08',
+  database: 'heroku_8042a9edba81ab0'
 })
 
 db.connect(err => {
