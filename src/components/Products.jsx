eea0514485d13f7a1602d20084a42e50de9c5d8b
Emily Tamayo <EmilyLeonella@R2Hs-MacBook-Pro.local>
2022-04-23 21:17:30 -0400

    fixed

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,51 +6,44 @@ import ProductTem from "./ProductTem";
 class Products extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { inventory:[], loading: false }; // dont load the data yet 
-  
+    this.state = { inventory: [], loading: false }; // dont load the data yet
   }
-  componentWillMount (){
+  componentWillMount() {
     fetch("https://dbecom.herokuapp.com/products")
-    .then((response)=>{
-      // this.setState({
-      //   products: response.status
-      // })
-      // this.state = response.status // storing the response inside this.state
-        console.log(response);
-        // console.log(response.json());
-        return response.json();
-    },
+      .then(
+        (response) => {
+          // this.setState({
+          //   products: response.status
+          // })
+          // this.state = response.status // storing the response inside this.state
+          console.log(response);
+          // console.log(response.json());
+          return response.json();
+        },
 
-    (error)=>{
-        this.setState({error:error});
-        console.log(error.status);
-    }
-    )
-    .then((data)=>{
-        // holds the state 
-        this.setState({inventory:data, loading: true}); // just setting the state , now load the data
-        console.log(this.state)
-    },
-    
-    );
+        (error) => {
+          this.setState({ error: error });
+          console.log(error.status);
+        }
+      )
+      .then((data) => {
+        // holds the state
+        this.setState({ inventory: data, loading: true }); // just setting the state , now load the data
+        console.log(this.state);
+      });
+  }
+
+  render() {
+    return this.state.inventory.map((e) => (
+      <ProductTem
+        key={e.id}
+        name={e.name}
+        description={e.description}
+        price={e.price}
+        image={e.images}
+      />
+    ));
+  }
 }
 
-render(){
-  return(
-    this.state.inventory.map(e => (  
-    <ProductTem
-    key={e.id}
-    name= {e.name}
-    description= {e.description}
-    price = {e.price}
-    image = {e.images}
-    />
-
-   ))
-  
-  
-  )
-}
-}
-
-  export default Products;
+export default Products;
